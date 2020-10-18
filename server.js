1d97e3f215ae87699901d7b57062cd37e2e28f3c
Ankit Chaudhary <you@example.com>
2020-10-18 17:21:24 +0530

    added gzipper

--- server.js
+++
@@ -1,8 +0,0 @@
-let express= require("express");
-var app = express();
-app.use(express.static('./dist/spaceX-launch'));
-app.get("/*", function(req, res) {
-    res.sendFile("index.html", {root: 'dist/spaceX-launch/'}
-  );
-  });
-  app.listen(process.env.PORT || 8080);
